42c1d3557e3b86e2e9a5b6c62709aa183864ee23
ridzuan5757 <ridzuan5757@protonmail.com>
2023-03-05 23:13:52 +0800

    first init

---
+++ mainwindow.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QString sPath = "C:/";
+
+    dirModel = new QFileSystemModel(this);
+    dirModel->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
+    dirModel->setRootPath(sPath);
+
+    ui->treeView->setModel(dirModel);
+    ui->treeView->setColumnHidden(0, false);
+    ui->treeView->setColumnHidden(1, true);
+    ui->treeView->setColumnHidden(2, true);
+    ui->treeView->setColumnHidden(3, true);
+
+    fileModel = new QFileSystemModel(this);
+    fileModel->setFilter(QDir::NoDotAndDotDot | QDir::Files);
+    fileModel->setRootPath(sPath);
+    ui->listView->setModel(fileModel);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_treeView_clicked(const QModelIndex &index)
+{
+    QString sPath = dirModel->fileInfo(index).absoluteFilePath();
+    ui->listView->setRootIndex(fileModel->setRootPath(sPath));
+}
+
